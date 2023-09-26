8346016e20bf6fee52da997179259ebabe636bdf
Andrii-888 <122465780+Andrii-888@users.noreply.github.com>
2023-09-26 17:28:56 +0200

    add paddings two elements

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,18 +1,29 @@
 import { Box, Grid, Link, Stack, SvgIcon, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { useTheme } from "@mui/material/styles";
+// import { useTheme } from "@mui/material/styles";
 
 import Image from "../../assets/map.png";
 import { ADDRESS, PHONE_NUMBER } from "../../constants";
-import { pxToRem } from "../../App";
 
 import { ReactComponent as Instagram } from "../../assets/instagram.svg";
 import { ReactComponent as Watsap } from "../../assets/watsap.svg";
+import { responsiveFontSizes } from "../../utils";
 
 const PhoneNumber = styled(Typography)(({ theme }) => ({
   fontWeight: theme.typography.fontWeightMedium,
-  fontSize: pxToRem(70),
   lineHeight: "91px",
+  margin: "25px 0 30px 0",
+  ...responsiveFontSizes({ sm: 50, md: 60, lg: 70 }),
+}));
+
+const Address = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.main,
+  fontWeight: theme.typography.fontWeightMedium,
+  textDecorationColor: theme.palette.text.main,
+  cursor: "pointer",
+  "&:hover": {
+    textDecorationColor: "#0069c2",
+  },
 }));
 
 const svgStyle = {
@@ -21,44 +32,44 @@ const svgStyle = {
 };
 
 function Footer() {
-  const { palette, typography } = useTheme();
+  // const { palette, typography } = useTheme();
   return (
-    <Box component="footer" sx={{ height: 933, p: "48px 32px 47px 32px" }}>
+    <Box component="footer" sx={{ p: "48px 32px 47px 32px" }}>
       <Grid container spacing={6}>
         <Grid container item xs={12}>
           <Grid item xs={6}>
             <Typography variant="h1">Contact</Typography>
+
             <PhoneNumber>{PHONE_NUMBER}</PhoneNumber>
+
             <Stack flexDirection="row" gap={8}>
-              <SvgIcon sx={svgStyle}>
-                <Instagram />
-              </SvgIcon>
-              <SvgIcon sx={svgStyle}>
-                <Watsap />
-              </SvgIcon>
+              <Stack gap={1} alignItems="center">
+                <SvgIcon sx={svgStyle}>
+                  <Instagram />
+                </SvgIcon>
+                <Typography variant="caption">Instagram</Typography>
+              </Stack>
+
+              <Stack gap={1} alignItems="center">
+                <SvgIcon sx={svgStyle}>
+                  <Watsap />
+                </SvgIcon>
+                <Typography variant="caption">WhatsApp</Typography>
+              </Stack>
             </Stack>
           </Grid>
           <Grid item xs={6} sx={{ paddingLeft: "142px" }}>
-            <Typography variant="h1">Address</Typography>
-            <Typography
-              variant="h1"
-              component={Link}
-              sx={{
-                color: palette.text.main,
-                fontWeight: typography.fontWeightMedium,
-                textDecorationColor: palette.text.main,
-                cursor: "pointer",
-                "&:hover": {
-                  textDecorationColor: "#0069c2",
-                },
-              }}
-            >
-              {ADDRESS}
+            <Typography variant="h1" sx={{ pb: "25px" }}>
+              Address
             </Typography>
-            <Typography variant="h5" sx={{ fontWeight: 500 }}>
+
+            <Address variant="h1" component={Link}>
+              {ADDRESS}
+            </Address>
+            <Typography variant="body2" sx={{ fontWeight: 500, pt: "30px" }}>
               Working Hours:
             </Typography>
-            <Typography variant="h3" sx={{ fontWeight: 700 }}>
+            <Typography variant="body1" sx={{ fontWeight: 700 }}>
               24 hours a day
             </Typography>
           </Grid>
---
+++ src/utils.js
@@ -0,0 +1,17 @@
+export const pxToRem = (value) => {
+  return `${value / 16}rem`;
+};
+
+export const responsiveFontSizes = ({ sm, md, lg }) => {
+  return {
+    "@media (min-width:600px)": {
+      fontSize: pxToRem(sm),
+    },
+    "@media (min-width:1200px)": {
+      fontSize: pxToRem(md),
+    },
+    "@media (min-width:1440px)": {
+      fontSize: pxToRem(lg),
+    },
+  };
+};
