4fb1efaea4175ae1a29bc430520452b754dbb36d
Andrii-888 <122465780+Andrii-888@users.noreply.github.com>
2023-10-16 17:17:58 +0200

    add put margin on the page components filters

--- src/pages/Products/Filters.jsx
+++ src/pages/Products/Filters.jsx
@@ -1,17 +1,40 @@
-import styled from "@emotion/styled";
-import { Box, Checkbox, Stack, TextField, Typography } from "@mui/material";
+import React from "react";
+import {
+  TextField,
+  Checkbox,
+  FormControl,
+  Select,
+  MenuItem,
+  Stack,
+  Typography,
+} from "@mui/material";
 
-const StyleTextField = styled(TextField)(({ theme }) => ({
-  width: "103px",
-  height: "22px",
-  "& .MuiInputBase-input": {
-    borderRadius: "7px",
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-    padding: "10px",
+const styles = {
+  input: {
+    width: "103px",
+    height: "42px",
+    // border: "1px solid #000000",
+    // color: "#000000",
+    // borderRadius: "7px",
   },
-}));
+  checkbox: {
+    width: "40px",
+    height: "40px",
+    // border: "1px solid #000000",
+    // borderRadius: "7px",
+  },
+  select: {
+    width: "219px",
+    height: "40px",
+    // border: "1px solid #000000",
+    // borderRadius: "7px",
+    color: "#000000",
+  },
+};
 
-const Filters = () => {
+function Filters() {
   return (
     <Stack
       flexDirection="row"
@@ -20,37 +43,35 @@ const Filters = () => {
         margin: "88.17px 0 54.2px 0",
       }}
     >
-      <Stack flexDirection="row" sx={{ alignItems: "center" }}>
-        <Typography
-          sx={{
-            fontSize: "1.25rem",
-            fontStyle: "normal",
-            fontWeight: "600",
-            lineHeight: "1.625rem",
-            letterSpacing: "0.0375rem",
-            color: "#000000",
-          }}
-        >
-          Price
-        </Typography>
+      <Typography
+        sx={{
+          alignItems: "center",
+          fontSize: "1.25rem",
+          fontStyle: "normal",
+          fontWeight: "600",
+          lineHeight: "1.625rem",
+          letterSpacing: "0.0375rem",
+          color: "#000000",
+          m: "88px 0 54.2px 30px",
+        }}
+      >
+        Price
+      </Typography>
 
-        <Box flexDirection="row" sx={{ alignItems: "center", height: "40px" }}>
-          <StyleTextField
-            label="from"
-            // variant="outlined"
-            size="small"
-            sx={{
-              marginLeft: "15px",
-            }}
-          />
-          <StyleTextField
-            label="to"
-            // variant="outlined"
-            size="small"
-            sx={{ marginLeft: "15px" }}
-          />
-        </Box>
-      </Stack>
+      <TextField
+        label="from"
+        variant="outlined"
+        size="small"
+        style={styles.input}
+        sx={{ m: "81px 0 54.2px 15px" }}
+      />
+      <TextField
+        label="to"
+        variant="outlined"
+        size="small"
+        style={styles.input}
+        sx={{ m: "81px 0 54.2px 15px" }}
+      />
 
       <Stack flexDirection="row" sx={{ alignItems: "center" }}>
         <Typography
@@ -61,23 +82,17 @@ const Filters = () => {
             lineHeight: "1.625rem",
             letterSpacing: "0.0375rem",
             color: "#000000",
-            margin: "0 39px 0 40px",
+            m: "81px 0 54px 40px",
           }}
         >
           Discounted items
         </Typography>
+
         <Checkbox
-          sx={{
-            color: "#000000",
-            borderWidth: "1px",
-            borderRadius: "7px",
-            // strokeWidth: "1px",
-            // "& .MuiSvgIcon-root": {
-            //   fontSize: 40,
-            height: "40px",
-            width: "40px",
-            // },
-          }}
+          {...label}
+          defaultChecked
+          style={styles.checkbox}
+          sx={{ m: "81px 0 47px 39px" }}
         />
       </Stack>
 
@@ -89,26 +104,26 @@ const Filters = () => {
             fontWeight: "600",
             lineHeight: "1.625rem",
             letterSpacing: "0.0375rem",
-            margin: "0 42px 0 91px",
+            color: "#000000",
+            m: "81px 0 47px 91px",
           }}
         >
           Sorted
         </Typography>
 
-        <Box width="219px">
-          <TextField
-            label="by default"
-            select
-            // value={prioduct}
-            // onChange={handleChange}
-            fullWidth
-            size="small"
-            color="secondary"
-          ></TextField>
-        </Box>
+        <FormControl
+          variant="outlined"
+          size="small"
+          style={styles.select}
+          sx={{ m: "81px 0 47px 42px" }}
+        >
+          <Select>
+            <MenuItem label="by default"></MenuItem>
+          </Select>
+        </FormControl>
       </Stack>
     </Stack>
   );
-};
+}
 
 export default Filters;
